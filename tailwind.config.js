265acae9c2ef8b4a9ea4e9817afb3d92abebfd4c
youichi Ishigaki <deepestred1968@gmail.com>
2022-03-09 22:30:07 +0900

    tailwind.config.js 修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@ module.exports = {
 	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: (theme) => ({
+				"like-button":
+					"url('https://icooon-mono.com/i/icon_14002/icon_140020_256.png')",
+			}),
+		},
 	},
 	variants: {
-		extend: {},
-	},
-	plugins: [],
-};
+		extend: {
+		backgroundImage: ['responsive', 'hover', 'focus'],
+		},
+		plugins: [],
+	}
+}
