e21b6b0d4cb2a13e90346b7926a48dce163fe5d5
youichi Ishigaki <deepestred1968@gmail.com>
2022-04-24 11:38:17 +0900

    start creating Login Page

---
+++ src/components/pages/Login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// type Props = {
+
+// }
+
+const Login : React.FC = () => {
+  return (
+    <div>
+      <h1>Hello Login</h1>
+    </div>
+  )
+}
+
+export default Login
